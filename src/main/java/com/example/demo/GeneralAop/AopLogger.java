ef4131018c78f759be3bf3a658ec6153a9d5d1ff
Moetez <moetezmaddouri@gmail.com>
2022-12-16 17:50:55 +0100

    calcule time of execution with @Around

--- src/main/java/com/example/demo/GeneralAop/AopLogger.java
+++ src/main/java/com/example/demo/GeneralAop/AopLogger.java
@@ -4,6 +4,7 @@ package com.example.demo.GeneralAop;
 import com.example.demo.Models.Employees;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -22,6 +23,11 @@ public class AopLogger {
     public void loggingMthodeGetEmploye(){
     }
 
+    // point de coupure sur la methode addEmployee
+    @Pointcut("execution(* com.example.demo.Controller.EmployeesContoller.addEmployee(..))")
+    public void logginAddEmployee(){
+    }
+
     @Before("logginPointCoupur()")
     public void beforCallEmployeeControllerMethodes(JoinPoint joinPoint){
         log.info("before call methode of Employee controller :"+joinPoint.getSignature());
@@ -32,6 +38,7 @@ public class AopLogger {
         log.info("after call : "+joinPoint.getSignature());
     }
 
+
     // get the return of methode getEmployees
     @AfterReturning(value = "loggingMthodeGetEmploye()",returning = "returnMethode")
     public void afterReturnin(JoinPoint joinPoint, ResponseEntity<Employees> returnMethode){
@@ -41,5 +48,13 @@ public class AopLogger {
     @AfterThrowing(value = "logginPointCoupur()",throwing = "exception")
     public void afterReturnin(JoinPoint joinPoint,Exception exception){
         log.info("exception  : "+exception.getMessage());
+    }
+    @Around(value = "logginPointCoupur()")
+    public Object loggiingAddEmployer(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+        long timeStart =System.currentTimeMillis();
+        Object object=proceedingJoinPoint.proceed();
+        long endTimeMethode=System.currentTimeMillis();
+        log.info("time of execution of "+proceedingJoinPoint.getSignature().getName()+ " methode is :"+(endTimeMethode -timeStart)+" ms");
+        return object;
     }
 }
