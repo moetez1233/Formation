78e850454da74c4f31c5bff6035c4bc81c759497
Moetez <moetezmaddouri@gmail.com>
2022-12-16 14:50:03 +0100

    add exception of bad request

--- src/main/java/com/example/demo/CustumException/GlobalException.java
+++ src/main/java/com/example/demo/CustumException/GlobalException.java
@@ -2,6 +2,7 @@ package com.example.demo.CustumException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,12 @@ public class GlobalException {
     @ExceptionHandler
     public ResponseEntity<ErrorObject> handlerValidationException(MethodArgumentNotValidException exception){
         ErrorObject errorObject=new ErrorObject(HttpStatus.BAD_REQUEST.value(),exception.getFieldError().getDefaultMessage(),System.currentTimeMillis());
+        return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorObject> handlerBadRequest(HttpMessageNotReadableException exception) {
+        ErrorObject errorObject=new ErrorObject(HttpStatus.BAD_REQUEST.value(),"Bad request",System.currentTimeMillis());
         return new ResponseEntity<ErrorObject>(errorObject,HttpStatus.BAD_REQUEST);
     }
 }
