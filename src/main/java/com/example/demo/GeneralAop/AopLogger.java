0fadd2bf82a514a6920a57ee3a2d38b999ae7795
Moetez <moetezmaddouri@gmail.com>
2022-12-16 17:13:43 +0100

    AOP :@AfterReturning ,@AfterThrowing

--- src/main/java/com/example/demo/GeneralAop/AopLogger.java
+++ src/main/java/com/example/demo/GeneralAop/AopLogger.java
@@ -1,13 +1,14 @@
 package com.example.demo.GeneralAop;
 
 
+import com.example.demo.Models.Employees;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Aspect
 @Component
@@ -17,6 +18,10 @@ public class AopLogger {
     @Pointcut("execution(* com.example.demo.Controller.*.*(..))")
     public void logginPointCoupur(){
     }
+    @Pointcut("execution(* com.example.demo.Controller.EmployeesContoller.getEmployees(..))")
+    public void loggingMthodeGetEmploye(){
+    }
+
     @Before("logginPointCoupur()")
     public void beforCallEmployeeControllerMethodes(JoinPoint joinPoint){
         log.info("before call methode of Employee controller :"+joinPoint.getSignature());
@@ -25,5 +30,16 @@ public class AopLogger {
     @After("logginPointCoupur()")
     public void afterCallEmployeeControllerMethode(JoinPoint joinPoint){
         log.info("after call : "+joinPoint.getSignature());
+    }
+
+    // get the return of methode getEmployees
+    @AfterReturning(value = "loggingMthodeGetEmploye()",returning = "returnMethode")
+    public void afterReturnin(JoinPoint joinPoint, ResponseEntity<Employees> returnMethode){
+        log.info("employe from la methode getEmployer is : "+joinPoint.getSignature());
+        log.info(returnMethode.toString());
+    }
+    @AfterThrowing(value = "logginPointCoupur()",throwing = "exception")
+    public void afterReturnin(JoinPoint joinPoint,Exception exception){
+        log.info("exception  : "+exception.getMessage());
     }
 }
