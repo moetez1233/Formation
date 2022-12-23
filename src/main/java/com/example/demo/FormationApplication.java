8ec8c3bb08a91fb46ce9b6bb90ae554702c653c3
Moetez <moetezmaddouri@gmail.com>
2022-12-23 17:50:02 +0100

    save loggin in local file

--- src/main/java/com/example/demo/FormationApplication.java
+++ src/main/java/com/example/demo/FormationApplication.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
-public class FormationApplication {
+public class FormationApplication implements ApplicationRunner {
+	private static final Logger logger = LogManager.getLogger(FormationApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FormationApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		logger.debug("debug message "); // ce message ne s'affiche pas car dons log4j2.xml notre level='info' and debug<info
+		logger.info("starter application debugger success");
+		logger.error("error message");
+	}
 }
