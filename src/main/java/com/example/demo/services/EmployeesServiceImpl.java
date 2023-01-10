c5399188ea202e9be4ffebc3028104c0ac3bb91c
moetez1233 <moetezmaddouri@gmail.com>
2023-01-10 15:14:05 +0100

    feature

--- src/main/java/com/example/demo/services/EmployeesServiceImpl.java
+++ src/main/java/com/example/demo/services/EmployeesServiceImpl.java
@@ -3,21 +3,28 @@ package com.example.demo.services;
 import com.example.demo.custumException.NotExist;
 import com.example.demo.domain.Employe;
 import com.example.demo.repositories.EmployeesRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+
 @Service
 public class EmployeesServiceImpl implements EmployService{
     @Autowired
     private EmployeesRepository employeesRepository;
+    private static final Logger logger = LogManager.getLogger(EmployeesServiceImpl.class);
 
     @Override
     @Transactional(readOnly = true)
     public Employe getUser(String nom) {
-        return employeesRepository.getEmploteeByidEmplo(nom).orElseThrow(()->new NotExist("user "+nom+ " not found"));
+        return employeesRepository.getEmploteeByidEmplo(nom)
+                .orElseThrow(()->
+                    new NotExist("user "+nom+ " not found")
+                );
     }
 
     @Override
